5fa4ca2699d2123a8738d3c33f685dadff11d5fc
Jacek Kunicki <jacek.kunicki@softwaremill.com>
2022-12-01 15:05:15 +0000

    Using Future[Option] (doesn't compile)

--- src/main/scala/org/kunicki/monad_transformers/FromScratch.scala
+++ src/main/scala/org/kunicki/monad_transformers/FromScratch.scala
@@ -1,18 +1,20 @@
 package org.kunicki.monad_transformers
 
+import scala.concurrent.{ExecutionContext, Future}
+
 case class User(id: Long, name: String)
 
 case class Company(id: Long, name: String)
 
 class FromScratch:
 
-  type Effect[A] = Option[A]
+  type Effect[A] = Future[Option[A]]
 
   private def findUserById(id: Long): Effect[User] = ???
 
   private def findCompanyByUser(user: User): Effect[Company] = ???
 
-  def findCompanyByUserId(id: Long): Effect[Company] =
+  def findCompanyByUserId(id: Long)(using ExecutionContext): Effect[Company] =
     for {
       user <- findUserById(id)
       company <- findCompanyByUser(user)
