6d09bc395c16dc74e3b245db3fcb69c5f7b51d2b
Jacek Kunicki <jacek.kunicki@softwaremill.com>
2022-12-01 15:15:05 +0000

    Using OptionInsideX with Functor, FlatMap and Applicative

--- src/main/scala/org/kunicki/monad_transformers/FromScratch.scala
+++ src/main/scala/org/kunicki/monad_transformers/FromScratch.scala
@@ -1,5 +1,7 @@
 package org.kunicki.monad_transformers
 
+import cats.{Applicative, FlatMap, Functor}
+
 import scala.concurrent.{ExecutionContext, Future}
 
 case class User(id: Long, name: String)
@@ -8,7 +10,7 @@ case class Company(id: Long, name: String)
 
 class FromScratch:
 
-  type Effect[A] = OptionInsideFuture[A]
+  type Effect[A] = OptionInsideX[A, Future]
 
   private def findUserById(id: Long): Effect[User] = ???
 
@@ -48,13 +50,13 @@ case class OptionInsideList[A](value: List[Option[A]]):
 
 case class OptionInsideX[A, X[_]](value: X[Option[A]]):
 
-  def map[B](f: A => B): OptionInsideX[B, X] =
-    OptionInsideX(value.map(_.map(f)))
+  def map[B](f: A => B)(using fx: Functor[X]): OptionInsideX[B, X] =
+    OptionInsideX(fx.map(value)(_.map(f)))
 
-  def flatMap[B](f: A => OptionInsideX[B, X]): OptionInsideX[B, X] =
+  def flatMap[B](f: A => OptionInsideX[B, X])(using fmx: FlatMap[X], ax: Applicative[X]): OptionInsideX[B, X] =
     OptionInsideX(
-      value.flatMap {
+      fmx.flatMap(value) {
         case Some(a) => f(a).value
-        case None => mx.pure(None)
+        case None => ax.pure(None)
       }
     )
