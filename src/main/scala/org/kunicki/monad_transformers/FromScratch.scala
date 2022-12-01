78c69165ab2ec706c2a9e9955e1ff8a4ce28e31c
Jacek Kunicki <jacek.kunicki@softwaremill.com>
2022-12-01 15:02:28 +0000

    API template

---
+++ src/main/scala/org/kunicki/monad_transformers/FromScratch.scala
@@ -0,0 +1,15 @@
+package org.kunicki.monad_transformers
+
+case class User(id: Long, name: String)
+
+case class Company(id: Long, name: String)
+
+class FromScratch:
+
+  type Effect[A] = A
+
+  private def findUserById(id: Long): Effect[User] = ???
+
+  private def findCompanyByUser(user: User): Effect[Company] = ???
+
+  def findCompanyByUserId(id: Long): Effect[Company] = ???
